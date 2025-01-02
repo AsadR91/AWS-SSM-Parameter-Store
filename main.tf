781e3239663abc540303027ffc70740b19acf5e0
arafique458 <arafique458@gmail.com>
2025-01-01 20:49:45 -0500

    Correctting the Error with the Terraform Infrastructure.

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-resource "aws_ssm_parameter" "parameters" {
-  count = length(var.parameters)
-  name = var.parameters[count.index].name
-  value = var.parameters[count.index].value
-  type = "String"
-  key_id = "e753-4178-ad43-a10be85deb97"
-  overwrite = true # will over-write the existing parameter and add any more.
-}
+# resource "aws_ssm_parameter" "parameters" {
+#   count = length(var.parameters)
+#   name = var.parameters[count.index].name
+#   value = var.parameters[count.index].value
+#   type = "String"
+#   key_id = "e753-4178-ad43-a10be85deb97"
+#   overwrite = true # will over-write the existing parameter and add any more.
+# }
 
-#resource "aws_ssm_parameter" "parameters" {
-#  count = length(var.parameters)
-#  name = var.parameters[count.index].name
-#  value = var.parameters[count.index].value
-#  overwrite = true # will over-write the existing parameter and add any more.
-#  type = "String"
-#  key_id = "801d82e5-19a3-4b80-88c6-5947b7b90508"
-#}
+resource "aws_ssm_parameter" "parameters" {
+ count = length(var.parameters)
+ name = var.parameters[count.index].name
+ value = var.parameters[count.index].value
+ overwrite = true # will over-write the existing parameter and add any more.
+ type = "String"
+ key_id = "8226817c-e753-4178-ad43-a10be85deb97"
+}
 #
 ### Passwords are not at all meant to keep inside the code at all, here sice it is a lab we are doing it to save the time.
-#resource "aws_ssm_parameter" "passwords" {
-#  count  = length(var.passwords)
-#  name   = var.passwords[count.index].name
-#  value  = var.passwords[count.index].value
-#  overwrite = true # will over-write the existing parameter and add any more.
-#  type = "SecureString"
-#  key_id = "801d82e5-19a3-4b80-88c6-5947b7b90508"
-#}
+resource "aws_ssm_parameter" "passwords" {
+ count  = length(var.passwords)
+ name   = var.passwords[count.index].name
+ value  = var.passwords[count.index].value
+ overwrite = true # will over-write the existing parameter and add any more.
+ type = "SecureString"
+ key_id = "8226817c-e753-4178-ad43-a10be85deb97"
+}

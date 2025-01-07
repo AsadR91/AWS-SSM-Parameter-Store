f8d3f8ddced6b8a1d04b899dc1219f338308146b
arafique458 <arafique458@gmail.com>
2025-01-06 23:48:10 -0500

    Added New KMS Key to test things out.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
  name   = var.parameters[count.index].name
  value  = var.parameters[count.index].value
  type   = "String"
- key_id = "8226817c-e753-4178-ad43-a10be85deb9"
+ key_id = "4f4d4c1d-a16f-4407-850d-c4584a6e2522"
 }
 
 ## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,7 +13,7 @@ resource "aws_ssm_parameter" "passwords" {
  name   = var.passwords[count.index].name
  value  = var.passwords[count.index].value
  type   = "SecureString"
- key_id = "8226817c-e753-4178-ad43-a10be85deb9"
+ key_id = "4f4d4c1d-a16f-4407-850d-c4584a6e2522"
 }
 
 # resource "aws_ssm_parameter" "parameters" {
@@ -22,7 +22,7 @@ resource "aws_ssm_parameter" "passwords" {
 #  value = var.parameters[count.index].value
 #  overwrite = true # will over-write the existing parameter and add any more.
 #  type = "String"
-#  key_id = "8226817c-e753-4178-ad43-a10be85deb97"
+#  key_id = "4f4d4c1d-a16f-4407-850d-c4584a6e25227"
 # }
 # #
 # ### Passwords are not at all meant to keep inside the code at all, here sice it is a lab we are doing it to save the time.
@@ -32,5 +32,5 @@ resource "aws_ssm_parameter" "passwords" {
 #  value  = var.passwords[count.index].value
 #  overwrite = true # will over-write the existing parameter and add any more.
 #  type = "SecureString"
-#  key_id = "8226817c-e753-4178-ad43-a10be85deb97"
+#  key_id = "4f4d4c1d-a16f-4407-850d-c4584a6e25227"
 # }
